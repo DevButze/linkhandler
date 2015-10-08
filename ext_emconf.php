14bd5e13978eea2240eeba23b86b10784069a650
Thorsten Boock <tboock@codegy.de>
2015-10-08 10:35:08 +0200

    [TASK] Raise supported TYPO3 versions.

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.2.0-7.4.99',
+			'typo3' => '7.5.0-7.5.99',
 		),
 		'conflicts' => array(
 			'ch_rterecords',
